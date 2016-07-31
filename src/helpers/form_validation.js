4bd750953b4940b9c9f322a8bb6d938017b04219
kulinich denis <kulinichdenis@kulinichs-MacBook-Pro.local>
2016-07-31 22:25:43 +0300

    update validate form

--- src/helpers/form_validation.js
+++ src/helpers/form_validation.js
@@ -1,8 +1,8 @@
 import yup from 'yup'
 
 export default yup.object({
-	name: yup.string().required('please enter Name'),
-	cost: yup.number().label('Cost').positive().min(1).max(9999999, 'max number 9999999').required('please enter Cost'),
+	name: yup.string().required('please enter name'),
+	cost: yup.number().label('Cost').positive().min(1).max(10000000, 'max Cost 10000000'),
 	percent: yup.string(),
 	group: yup.string()  
 });
